ed6aab91b657c8937e5c755b902feb4faf6fdba7
akash-ops <akashshetty602@gmail.com>
2024-03-04 19:42:06 +0530

    login screen ui with shadcn ui and validations inbuilt

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,35 +1,5 @@
 
 
-// import React, { useState } from 'react'
-// import Lottie from 'react-lottie'
-// import * as animationData from '../assets/animations/home_side.json'
-// const HomeScreen = () => {
-//     const defaultOptions = {
-//         loop: true,
-//         autoplay: true, 
-//         animationData: animationData,
-//         rendererSettings: {
-//           preserveAspectRatio: 'xMidYMid slice'
-//         }
-//       };
-//   return (
-//     <div className='grid place-items-center bg-slate-500'>
-//         {/* info container */}
-//         <div className='text-center px-10 mt-10 py-10 w-full flex flex-row justify-evenly items-end bg-gradient-to-b from-slate-500 to-slate-900'>
-//             <h2 className='w-1/2 text-white'>
-//             FinTrack is your ultimate solution for effortlessly managing expenses and savings, offering a seamless online platform to track your financial journey with precision and ease. Whether you're a seasoned investor or just starting to budget, FinTrack empowers you to take control of your finances with confidence.
-        
-//             </h2>
-//             <Lottie options={defaultOptions} width={400}/>
-//         <div>
-//         </div>
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default HomeScreen
-
 "use client";
 import React from "react";
 import { Boxes } from "./ui/background-boxes";
